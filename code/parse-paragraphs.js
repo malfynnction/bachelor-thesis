02e775b53ba0730724f4fc975c87309f62cf26e3
Fynn Heintz <fynn.heintz@gmail.com>
2020-01-06 16:28:29 +0100

    add count of sentences included in Naderi dataset

--- code/parse-paragraphs.js
+++ code/parse-paragraphs.js
@@ -28,3 +28,31 @@ const removeFootnoteReferences = () => {
 
   xlsx.writeFile(workbook, filePath)
 }
+
+const countNaderiSentences = () => {
+  const countColumn = 'I'
+  const naderiSentences = getColumn('../Full-Dataset-Naderi19.xlsx', 'Sentences', 'C').map(({cell, value}) => value)
+
+  const count = {}
+
+  naderiSentences.forEach(sentence => {
+    const includingParagraph = paragraphs.find(({cell, value}) => value.includes(sentence))
+    if (typeof includingParagraph !== 'undefined') {
+      const { cell } = includingParagraph
+      count[cell] = (count[cell] || 0) + 1
+    }
+  })
+
+  const cells = paragraphs.map(({cell, value}) => cell)
+  cells.forEach(cell => {
+    const countCell = cell.replace(paragraphColumn, countColumn)
+    sheet[countCell] = {
+      t: 'n', // type: number
+      v: count[cell] || 0
+    }
+  })
+
+  xlsx.writeFile(workbook, filePath)
+}
+
+countNaderiSentences()
