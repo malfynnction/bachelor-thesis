94a4876389cbc54222a508b409ffb8b2ab08bca8
Fynn Heintz <fynn.heintz@gmail.com>
2020-01-19 09:41:26 +0100

    color adjusments

--- ui/src/components/Start.js
+++ ui/src/components/Start.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Start = () => {
   return (
-    <div className="tu-border center-box background-pink">
+    <div className="tu-border center-box">
       <h2>Welcome to this session!</h2>
       <div>Thank you for helping us.</div>
       <Link to="/Instructions">Start</Link>
