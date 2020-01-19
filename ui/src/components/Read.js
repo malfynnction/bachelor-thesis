f2b6c7735df3b13967cc4ceba03a75c431db3472
Fynn Heintz <fynn.heintz@gmail.com>
2020-01-19 13:38:04 +0100

    proper toggle of text items while reading

--- ui/src/components/Read.js
+++ ui/src/components/Read.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import Timer from 'react-compound-timer'
+import '../styles/Read.css'
 
 class Read extends React.Component {
   constructor(props) {
@@ -9,7 +10,7 @@ class Read extends React.Component {
 
   revealItem(timerControl) {
     timerControl.start()
-    this.setState({ showItem: true }, () => {})
+    this.setState({ showItem: true })
   }
 
   hideItem(timerControl) {
@@ -24,18 +25,26 @@ class Read extends React.Component {
         <div>Here is a sentence/paragraph that you should read: </div>
         <Timer startImmediately={false} timeToUpdate={100}>
           {timerControl => (
-            <button
-              onTouchStart={() => this.revealItem(timerControl)}
-              onTouchEnd={() => this.hideItem(timerControl, Timer)}
-              onMouseDown={() => this.revealItem(timerControl)}
-              onMouseUp={() => this.hideItem(timerControl, Timer)}
-              onMouseLeave={() => this.hideItem(timerControl, Timer)}
-            >
-              Show me
-            </button>
+            <Fragment>
+              <div
+                className={`item-text centered-content ${
+                  this.state.showItem ? 'display' : 'hidden'
+                }`}
+              >
+                <p>{this.props.item}</p>
+              </div>
+              <button
+                onTouchStart={() => this.revealItem(timerControl)}
+                onTouchEnd={() => this.hideItem(timerControl, Timer)}
+                onMouseDown={() => this.revealItem(timerControl)}
+                onMouseUp={() => this.hideItem(timerControl, Timer)}
+                onMouseLeave={() => this.hideItem(timerControl, Timer)}
+              >
+                Show me
+              </button>
+            </Fragment>
           )}
         </Timer>
-        {this.state.showItem ? <p>{this.props.item}</p> : null}
       </Fragment>
     )
   }
