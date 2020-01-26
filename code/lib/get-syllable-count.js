09d11a3e3b779ab121cd9d96206802c7869ba59c
Fynn Heintz <fynn.heintz@gmail.com>
2020-01-26 15:44:07 +0100

    estimate the syllables for the unknown words

--- code/lib/get-syllable-count.js
+++ code/lib/get-syllable-count.js
@@ -1,10 +1,12 @@
 const request = require('request-promise-native')
 const { parse } = require('node-html-parser')
-const readline = require('readline')
 const fs = require('fs')
 
 const syllableCounts = JSON.parse(fs.readFileSync('./lib/syllable-counts.json'))
-const punctuation = /\.|,|-|\(|\)|\/|"|;|:|…|„|“/g
+const punctuation = /\.|!|,|-|\(|\)|\/|"|;|:|…|„|“/g
+const vowel = /^a|[^a]a|^e|[^aeiou]e|^i|[^i]i|^o|[^o]o|^u|[^eu]u|ä|ö|ü/g
+
+const estimateSyllables = word => (word.match(vowel) || ['']).length
 
 module.exports = word => {
   const parsedWord = word
@@ -15,9 +17,14 @@ module.exports = word => {
     .replace(punctuation, '')
 
   return new Promise((resolve, reject) => {
+    // Ignore words that are already cached
+    if (typeof syllableCounts[parsedWord.toLowerCase()] !== 'undefined') {
+      resolve(syllableCounts[parsedWord.toLowerCase()])
+    }
+
     request
       .get(`https://www.duden.de/rechtschreibung/${parsedWord}`)
-      .then(res => {
+      .then(async res => {
         const dom = parse(res)
         const rechtschreibung = dom.querySelector('#rechtschreibung')
         let separation
@@ -37,30 +44,21 @@ module.exports = word => {
           }
         }
         const syllables = separation.split(',')[0].split('|').length
+        syllableCounts[parsedWord.toLowerCase()] = syllables
+        await fs.writeFileSync(
+          './lib/syllable-counts.json',
+          JSON.stringify(syllableCounts)
+        )
         resolve(syllables)
       })
-      .catch(err => {
-        if (typeof syllableCounts[parsedWord.toLowerCase()] !== 'undefined') {
-          resolve(syllableCounts[parsedWord.toLowerCase()])
-        } else {
-          const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-          })
-
-          rl.question(
-            `I'm having problems with the word "${parsedWord}". How many syllables does it have? `,
-            async syllables => {
-              syllableCounts[parsedWord.toLowerCase()] = syllables
-              await fs.writeFileSync(
-                './lib/syllable-counts.json',
-                JSON.stringify(syllableCounts)
-              )
-              rl.close()
-              resolve(syllables)
-            }
-          )
-        }
+      .catch(async err => {
+        const syllables = estimateSyllables(word)
+        syllableCounts[parsedWord.toLowerCase()] = syllables
+        await fs.writeFileSync(
+          './lib/syllable-counts.json',
+          JSON.stringify(syllableCounts)
+        )
+        resolve(syllables)
       })
   })
 }
