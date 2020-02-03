3fd6c0f89e237317da977be94f856d5eddeafa7d
Fynn Heintz <fynn.heintz@gmail.com>
2020-02-03 13:40:23 +0100

    separate data and code

--- ui/src/components/Questions.js
+++ ui/src/components/Questions.js
@@ -1,110 +1,104 @@
 import React, { Fragment } from 'react'
 import '../styles/Questions.css'
 
-const Question = props => {
-  return (
-    <div className="question-box">
-      <div>
-        <strong>{props.question.label}</strong>
-      </div>
-      <div className={`${props.horizontal ? 'horizontal-answer-box' : ''}`}>
-        {props.answers.map(({ label, value }, i) => {
-          return (
-            <div
-              key={`${props.question.key}-${value}`}
-              className={`${props.horizontal ? 'horizontal-answer' : ''}`}
-            >
-              <input
-                onChange={() => props.onChange(value)}
-                type="radio"
-                name={props.question.key}
-                id={`${props.question.key}-${value}`}
-                value={value}
-                checked={value === props.checkedAnswer}
-              />
-              {props.horizontal ? <br /> : null}
-              <label htmlFor={`${props.question.key}-${value}`}>{label}</label>
-            </div>
-          )
-        })}
-      </div>
-    </div>
-  )
-}
+const allQuestions = [
+  {
+    key: 'q0',
+    label: 'Is this the first question?',
+    answers: [
+      { label: 'yes', value: 'y' },
+      { label: 'no', value: 'n' },
+      { label: 'why do you need three possible answers?', value: 'why' },
+      { label: "fuck the binary, that's why", value: 'nb' },
+    ],
+    horizontalAnswers: false,
+  },
+  {
+    key: 'q1',
+    label: 'Do you know what happens when I add more questions?',
+    answers: [
+      { label: 'yes', value: 'y' },
+      { label: 'no', value: 'n' },
+      { label: "I don't even care", value: 'idc' },
+    ],
+    horizontalAnswers: false,
+  },
+  {
+    key: 'q2',
+    label: 'Question?',
+    answers: [
+      { label: 'answer 1', value: 1 },
+      { label: 'answer 2', value: 2 },
+      { label: 'answer 3', value: 3 },
+      { label: 'answer 4', value: 4 },
+      { label: 'answer 5', value: 5 },
+      { label: 'answer 6', value: 6 },
+    ],
+    horizontalAnswers: false,
+  },
+  {
+    key: 'q3',
+    label: 'Did you have to scroll to see this?',
+    answers: [
+      { label: 'Yes', value: 'y' },
+      { label: 'No, your site is broken', value: 'damn' },
+      { label: 'No, I have a big screen', value: 'big' },
+      { label: "I can't see this", value: 'what' },
+    ],
+    horizontalAnswers: false,
+  },
+  {
+    key: 'q4',
+    label: 'Did you know I can also arrange the answers differently?',
+    answers: [
+      { label: 'Yes', value: 'y' },
+      { label: '', value: 'y-idk' },
+      { label: "I don't know", value: 'idk' },
+      { label: '', value: 'idk-n' },
+      { label: 'No', value: 'n' },
+    ],
+    horizontalAnswers: true,
+  },
+]
 
 const Questions = props => {
   return (
     <Fragment>
       <div>Now answer some questions about what you just read</div>
-      <Question
-        question={{ label: 'Is this the first question?', key: 'q0' }}
-        answers={[
-          { label: 'yes', value: 'y' },
-          { label: 'no', value: 'n' },
-          { label: 'why do you need three possible answers?', value: 'why' },
-          { label: "fuck the binary, that's why", value: 'nb' },
-        ]}
-        checkedAnswer={props.answers['q0']}
-        onChange={value => {
-          props.onChange('q0', value)
-        }}
-      />
-      <Question
-        question={{
-          label: 'Do you know what happens when I add more questions?',
-          key: 'q1',
-        }}
-        answers={[
-          { label: 'yes', value: 'y' },
-          { label: 'no', value: 'n' },
-          { label: "I don't even care", value: 'idc' },
-        ]}
-        checkedAnswer={props.answers['q1']}
-        onChange={value => props.onChange('q1', value)}
-      />
-      <Question
-        question={{
-          label: 'Question?',
-          key: 'q2',
-        }}
-        answers={[
-          { label: 'answer 1', value: 1 },
-          { label: 'answer 2', value: 2 },
-          { label: 'answer 3', value: 3 },
-          { label: 'answer 4', value: 4 },
-          { label: 'answer 5', value: 5 },
-          { label: 'answer 6', value: 6 },
-        ]}
-        checkedAnswer={props.answers['q2']}
-        onChange={value => props.onChange('q2', value)}
-      />
-      <Question
-        question={{ label: 'Did you have to scroll to see this?', key: 'q3' }}
-        answers={[
-          { label: 'Yes', value: 'y' },
-          { label: 'No, your site is broken', value: 'damn' },
-          { label: 'No, I have a big screen', value: 'big' },
-          { label: "I can't see this", value: 'what' },
-        ]}
-        checkedAnswer={props.answers['q3']}
-        onChange={value => props.onChange('q3', value)}
-      />
-      <Question
-        question={{
-          label: 'Did you know I can also arrange the answers differently?',
-          key: 'q4',
-        }}
-        answers={[
-          { label: 'Yes', value: 'y' },
-          { label: '', value: 'y-idk' },
-          { label: "I don't know", value: 'idk' },
-          { label: '', value: 'idk-n' },
-          { label: 'No', value: 'n' },
-        ]}
-        checkedAnswer={props.answers['q4']}
-        onChange={value => props.onChange('q4', value)}
-        horizontal={true}
-      />
+      {allQuestions.map(({ key, label, answers, horizontalAnswers }) => (
+        <Fragment>
+          <div className="question-box">
+            <div>
+              <strong>{label}</strong>
+            </div>
+            <div
+              className={`${horizontalAnswers ? 'horizontal-answer-box' : ''}`}
+            >
+              {answers.map(({ label, value }, i) => {
+                return (
+                  <div
+                    key={`${key}-${value}`}
+                    className={`${
+                      horizontalAnswers ? 'horizontal-answer' : ''
+                    }`}
+                  >
+                    <input
+                      onChange={() => props.onChange(key, value)}
+                      type="radio"
+                      name={key}
+                      id={`${key}-${value}`}
+                      value={value}
+                      checked={value === props.answers[key]}
+                    />
+                    {horizontalAnswers ? <br /> : null}
+                    <label htmlFor={`${key}-${value}`}>{label}</label>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </Fragment>
+      ))}
     </Fragment>
   )
 }
