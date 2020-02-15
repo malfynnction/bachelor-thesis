52c46ca0906637a429e5e65f531c4b73b7c24c31
Fynn Heintz <fynn.heintz@gmail.com>
2020-02-15 12:33:24 +0100

    load texts from file dynamically

--- code/process-texts.py
+++ code/process-texts.py
@@ -1,21 +1,44 @@
 import spacy
+from openpyxl import load_workbook
 
 nlp = spacy.load('de')
 
-text = "Dies ist ein Satz. Das ist ein zweiter Satz, der das Ganze hier zu einem Paragraph macht.Ich füge einen 3. Satz ein, der im 19. Jh. am 27. Aug. geschrieben wurde."
-doc = nlp(text)
+def customSentenceBoundaries(doc):
+  notSentenceBoundary = [":", "°", ";", "±"]
+  for token in doc[:-1]:
+    if token.text in notSentenceBoundary:
+      doc[token.i+1].is_sent_start = False
+    if token.text == "°":
+      doc[token.i].is_sent_start = False
+    if token.text == "…" and doc[token.i-1].text == "[" and doc[token.i+1].text =="]": # ellipses in a quote are not a new sentence
+      doc[token.i+1].is_sent_start = False
+  return doc
 
+nlp.add_pipe(customSentenceBoundaries, before="parser")
 
-def tagPartsOfSpeech():
+dataPath = 'data.xlsx' # TODO: make this easier editable
+sheetName = 'paragraphs' # TODO: make this easier editable
+
+workbook = load_workbook(dataPath)
+sheet = workbook[sheetName]
+
+paragraphColumn = 'B'
+
+def getParsedTexts():
+  column = sheet[paragraphColumn]
+  texts = []
+  for cell in column:
+    texts.append(npl(cell.value))
+  return texts[1:] # texts[0] is the column header
+
+def tagPartsOfSpeech(text):
   words = []
-  for t in doc:
+  for t in text:
     words.append({'word': t.orth_, 'type': t.pos_})
   return words
 
-def separateSentences():
+def separateSentences(text):
   sentences = []
-  for sentence in doc.sents:
+  for sentence in text.sents:
     sentences.append(sentence)
   return sentences
-
-separateSentences()
