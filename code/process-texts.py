a7e40b732ca1ec234f355b5bf799a47d25b5eb88
Fynn Heintz <fynn.heintz@gmail.com>
2020-02-15 11:03:17 +0100

    separate sentences

--- code/process-texts.py
+++ code/process-texts.py
@@ -2,13 +2,20 @@ import spacy
 
 nlp = spacy.load('de')
 
-doc = nlp(u'Dies ist ein Satz. Das ist ein zweiter Satz, der das Ganze hier zu einem Paragraph macht.')
+text = "Dies ist ein Satz. Das ist ein zweiter Satz, der das Ganze hier zu einem Paragraph macht.Ich füge einen 3. Satz ein, der im 19. Jh. am 27. Aug. geschrieben wurde."
+doc = nlp(text)
 
 
-def partOfSpeechTagger():
+def tagPartsOfSpeech():
   words = []
   for t in doc:
     words.append({'word': t.orth_, 'type': t.pos_})
-  print(words)
+  return words
 
-partOfSpeechTagger()
+def separateSentences():
+  sentences = []
+  for sentence in doc.sents:
+    sentences.append(sentence)
+  return sentences
+
+separateSentences()
