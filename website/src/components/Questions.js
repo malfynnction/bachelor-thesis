c1df34348b634136df1f6f6673cc1cc7994887b6
Fynn Heintz <fynn.heintz@gmail.com>
2020-03-22 13:25:53 +0100

    show answer scales as radio buttons instead of using theFragebogen

--- website/src/components/Questions.js
+++ website/src/components/Questions.js
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import '../styles/Questions.css'
 import { itemPropType } from '../lib/prop-types'
-import { QuestionnaireItemSVGQuality7pt } from '../lib/thefragebogen'
 
 const getAllQuestions = (questionType, { type, sentences }) => {
   if (questionType === 'general') {
@@ -11,39 +10,39 @@ const getAllQuestions = (questionType, { type, sentences }) => {
         key: 'readability',
         label: `How difficult was it for you to read this ${type}?`,
         answers: [
-          { label: 'very difficult (1)', value: 1 },
-          { label: 'difficult (2)', value: 2 },
-          { label: 'slightly difficult (3)', value: 3 },
-          { label: 'neutral (4)', value: 4 },
-          { label: 'slightly easy (5)', value: 5 },
-          { label: 'easy (6)', value: 6 },
-          { label: 'very easy (7)', value: 7 },
+          { label: 'very difficult', value: 7 },
+          { label: 'difficult', value: 6 },
+          { label: 'slightly difficult', value: 5 },
+          { label: 'neutral', value: 4 },
+          { label: 'slightly easy', value: 3 },
+          { label: 'easy', value: 2 },
+          { label: 'very easy', value: 1 },
         ],
       },
       {
         key: 'complexity',
         label: `How do you rate the overall complexity of this ${type}?`,
         answers: [
-          { label: 'very complex (1)', value: 1 },
-          { label: 'complex (2)', value: 2 },
-          { label: 'slightly complex (3)', value: 3 },
-          { label: 'neutral (4)', value: 4 },
-          { label: 'slightly easy (5)', value: 5 },
-          { label: 'easy (6)', value: 6 },
-          { label: 'very easy (7)', value: 7 },
+          { label: 'very complex', value: 7 },
+          { label: 'complex', value: 6 },
+          { label: 'slightly complex', value: 5 },
+          { label: 'neutral', value: 4 },
+          { label: 'slightly easy', value: 3 },
+          { label: 'easy', value: 2 },
+          { label: 'very easy', value: 1 },
         ],
       },
       {
         key: 'understandability',
         label: `How well did you understand the ${type}?`,
         answers: [
-          { label: "didn't understand at all (1)", value: 1 },
-          { label: 'understood almost nothing (2)', value: 2 },
-          { label: 'understood only a few things (3)', value: 3 },
-          { label: 'understood some things (4)', value: 4 },
-          { label: 'undestood it relatively well (5)', value: 5 },
-          { label: 'understood most of it (6)', value: 6 },
-          { label: 'fully understood (7)', value: 7 },
+          { label: "didn't understand at all", value: 7 },
+          { label: 'understood almost nothing', value: 6 },
+          { label: 'understood only a few things', value: 5 },
+          { label: 'understood some things', value: 4 },
+          { label: 'undestood it relatively well', value: 3 },
+          { label: 'understood most of it', value: 2 },
+          { label: 'fully understood', value: 1 },
         ],
       },
     ]
@@ -63,129 +62,42 @@ const getAllQuestions = (questionType, { type, sentences }) => {
   }
 }
 
-class Questions extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      questions: getAllQuestions(props.questionType, props.item),
-      questionnaireItems: {},
-    }
-  }
-
-  componentDidMount() {
-    this.renderFragebogen()
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.item._id !== this.props.item._id) {
-      this.renderFragebogen()
-    }
-  }
-
-  renderFragebogenDivs() {
-    return (
-      <Fragment>
-        <div className="note">
-          (You can choose your answer by clicking anywhere on the scale)
-        </div>
-        {this.state.questions.map(({ key }) => {
-          const uniqueKey = `${this.props.item._id}-${key}`
-          return (
-            <div
-              key={uniqueKey}
-              id={uniqueKey}
-              className="question-box"
-              onClick={() => {
-                this.props.onChange(key, this.getAnswer(uniqueKey))
-              }}
-            />
-          )
-        })}
-      </Fragment>
-    )
-  }
-
-  renderFragebogen() {
-    const questionnaireItems = {}
-    this.state.questions.forEach(({ key, label, answers }) => {
-      const uniqueKey = `${this.props.item._id}-${key}`
-
-      const questionnaireItem = new QuestionnaireItemSVGQuality7pt(
-        '',
-        `<strong>${label}</strong>`,
-        false,
-        answers.map(answer => answer.label)
-      )
-
-      questionnaireItems[uniqueKey] = questionnaireItem
-
-      const parent = document.getElementById(uniqueKey)
-      if (parent && parent.childElementCount === 0) {
-        parent.appendChild(questionnaireItem.createUI())
-      }
-    })
-    this.setState({
-      questionnaireItems: {
-        ...questionnaireItems,
-      },
-    })
-  }
-
-  renderRadioButtons() {
-    return this.state.questions.map(({ key, label, answers }) => {
-      return (
-        <Fragment key={`question-${key}`}>
-          <div className="question-box">
-            <div>
-              <strong>{label}</strong>
+const Questions = props => {
+  return (
+    <Fragment>
+      <div>
+        Please answer some questions about the {props.item.type} you just read:
+      </div>
+      {getAllQuestions(props.questionType, props.item).map(
+        ({ key, label, answers }) => (
+          <Fragment key={`question-${key}`}>
+            <div className="question-box">
+              <div>
+                <strong>{label}</strong>
+              </div>
+              {answers.map(({ label, value }) => {
+                return (
+                  <div key={`${key}-${value}`} className="questionnaire-item">
+                    <input
+                      onChange={() => props.onChange(key, value)}
+                      type="radio"
+                      name={key}
+                      id={`${key}-${value}`}
+                      value={value}
+                      checked={value === props.answers[key]}
+                    />
+                    <label htmlFor={`${key}-${value}`}>
+                      {label} ({value})
+                    </label>
+                  </div>
+                )
+              })}
             </div>
-            {answers.map(({ label, value }) => {
-              return (
-                <div key={`${key}-${value}`} className="questionnaire-item">
-                  <input
-                    type="radio"
-                    name={key}
-                    id={`${key}-${value}`}
-                    value={value}
-                    checked={value === this.props.answers[key]}
-                    onChange={() => this.props.onChange(key, value)}
-                  />
-                  <label htmlFor={`${key}-${value}`}>{label}</label>
-                </div>
-              )
-            })}
-          </div>
-        </Fragment>
-      )
-    })
-  }
-
-  getAnswer(key) {
-    const questionnaireItem = this.state.questionnaireItems[key]
-    if (questionnaireItem) {
-      return questionnaireItem.getAnswer()
-    }
-  }
-
-  render() {
-    return (
-      <Fragment>
-        <div>
-          <strong>
-            Please answer{' '}
-            {this.state.questions.length > 1
-              ? 'these questions'
-              : 'this question'}{' '}
-            about the {this.props.item.type} you read:
-          </strong>
-        </div>
-
-        {this.props.questionType === 'general'
-          ? this.renderFragebogenDivs()
-          : this.renderRadioButtons()}
-      </Fragment>
-    )
-  }
+          </Fragment>
+        )
+      )}
+    </Fragment>
+  )
 }
 
 Questions.propTypes = {
