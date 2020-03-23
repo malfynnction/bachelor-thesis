6d12015a74ff969025d0328a76fd2e1bdd0ec29c
Fynn Heintz <fynn.heintz@gmail.com>
2020-03-23 11:47:58 +0100

    redirect crowdsourcing participants to CS platform instead of allowing another session

--- website/src/components/StartSession.js
+++ website/src/components/StartSession.js
@@ -46,59 +46,68 @@ class startSession extends React.Component {
     const { previousSession } = this.state
     const previouslyTraining = previousSession === 'Training'
     const previouslyRating = previousSession && !previouslyTraining
+    const allowAnotherSession = !this.state.token
 
     let thankYou = ''
     if (previouslyTraining) {
       thankYou = `
-        You have successfully submitted your test rating.
+        <span>You have successfully submitted your test rating.
         You can now start an actual rating session below.
         If you have any questions, you can read the
         <a href='/instructions'>Instructions</a>
-        again or contact us at TODO
+        again or contact us at TODO</span>
       `
     } else if (previousSession) {
-      thankYou += 'Thank you! Your answers have been saved. '
+      thankYou += '<span>Thank you!</span> <span>Your answers have been saved. '
       if (this.state.token) {
-        thankYou += `Your confirmation code is ${this.state.token}. `
+        thankYou += `Your confirmation code is <strong>${this.state.token}</strong>, please copy it and paste it in the corresponding box on the TODO website. `
       }
-      thankYou +=
-        'You can now close this window or start another session below:'
+      if (allowAnotherSession) {
+        thankYou +=
+          'You can now close this window or start another session below:'
+      } else {
+        thankYou += 'You can now close this window.'
+      }
+      thankYou += '</span>'
     }
     return (
       <div className="tu-border tu-glow center-box centered-content">
         <h2>Start {previouslyRating ? 'another' : 'a'} session</h2>
         {thankYou ? (
-          <div dangerouslySetInnerHTML={{ __html: thankYou }} />
+          <div
+            className="centered-content centered-text"
+            dangerouslySetInnerHTML={{ __html: thankYou }}
+          />
         ) : null}
-        <div>
-          {this.state.completedTrainingSession
-            ? `You can start ${
-                previouslyRating ? 'another' : 'a'
-              } rating session or do ${
-                previouslyTraining ? 'another' : 'a little'
-              } training session before. `
-            : 'Please go through a training session before you start with actual ratings. '}
-          A training session is just like a real session, except your answers
-          won't be recorded and you will get a pre-defined set of one very easy,
-          one medium, and one very difficult text.
-        </div>
-        {this.state.hasActiveSession ? (
-          <div>
-            You have unsaved ratings. You can continue where you left off and
-            complete your session, or start a new one and delete the unsaved
-            changes.
-          </div>
-        ) : null}
-        <div>
-          {this.state.hasActiveSession ? (
-            <Link className="btn" to="/session">
-              Continue Session
-            </Link>
-          ) : null}
-          {this.state.token ? (
-            <div></div>
-          ) : (
-            <Fragment>
+        {allowAnotherSession ? (
+          <Fragment>
+            <div>
+              {this.state.completedTrainingSession
+                ? `You can start ${
+                    previouslyRating ? 'another' : 'a'
+                  } rating session or do ${
+                    previouslyTraining ? 'another' : 'a little'
+                  } training session before. `
+                : 'Please go through a training session before you start with actual ratings. '}
+              A training session is just like a real session, except your
+              answers won't be recorded and you will get a pre-defined set of
+              one very easy, one medium, and one very difficult text.
+            </div>
+
+            {this.state.hasActiveSession ? (
+              <div>
+                You have unsaved ratings. You can continue where you left off
+                and complete your session, or start a new one and delete the
+                unsaved changes.
+              </div>
+            ) : null}
+            <div>
+              {this.state.hasActiveSession ? (
+                <Link className="btn" to="/session">
+                  Continue Session
+                </Link>
+              ) : null}
+
               {this.state.completedTrainingSession ? (
                 <Link
                   className="btn"
@@ -120,9 +129,9 @@ class startSession extends React.Component {
               >
                 Training Session
               </Link>
-            </Fragment>
-          )}
-        </div>
+            </div>
+          </Fragment>
+        ) : null}
       </div>
     )
   }
