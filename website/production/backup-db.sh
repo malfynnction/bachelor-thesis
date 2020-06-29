c2d91ae13f4db9ee9138e925449a578ba31778a7
Fynn Heintz <fynn.heintz@gmail.com>
2020-06-29 14:27:32 +0200

    create backups of the database

--- website/production/backup-db.sh
+++ website/production/backup-db.sh
@@ -1,2 +1,11 @@
-# TODO: dump couchdb/data (where?)
-# TODO: delete backups that are older than 1 week(?)
+BASE_DIR=/home/adminuser
+BACKUP_DIR=$BASE_DIR/db-backups
+STORAGE_LENGTH=14
+
+DATE=`date +%Y-%m-%d`
+FILENAME="db-backup_${DATE}.tar"
+tar -cvf $BACKUP_DIR/$FILENAME $BASEDIR/production/couchdb/data/
+# TODO: store backup (where?)
+
+# delete files older than $STORAGE_LENGTH days
+find $BACKUP_DIR -type f -mtime +$STORAGE_LENGTH -exec rm -f {} \;
