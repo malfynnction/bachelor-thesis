ceb5ae45e58d7bbbda93c3e0e989a6d987633019
Fynn Heintz <fynn.heintz@gmail.com>
2020-07-06 14:54:22 +0200

    fix: use placeholder values for now

--- website/frontend/src/config.js
+++ website/frontend/src/config.js
@@ -1,4 +1,4 @@
 export const CONTACT_MAIL = 'f.heintz@campus.tu-berlin.de'
-export const COMPENSATION = '2€ Amazon gift card'
-export const SESSIONS_PER_COMPENSATION = 5
-export const TEXTS_PER_SESSION = 10
+export const COMPENSATION = 'TODO€ Amazon gift card'
+export const SESSIONS_PER_COMPENSATION = 'TODO'
+export const TEXTS_PER_SESSION = 'TODO'
