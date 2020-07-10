4b8c335d303ceb3a3c707f470d023e1aac5679da
Fynn Heintz <fynn.heintz@gmail.com>
2020-07-10 10:55:34 +0200

    fix typo

--- website/production/bin/backup-db.sh
+++ website/production/bin/backup-db.sh
@@ -1,10 +1,10 @@
-BASE_DIR=/home/adminuser
-BACKUP_DIR=$BASE_DIR/db-backups
+BASE_DIR="/home/adminuser"
+BACKUP_DIR="${BASE_DIR}/db-backups"
 STORAGE_LENGTH=14
 
 DATE=`date +%Y-%m-%d`
 FILENAME="db-backup_${DATE}.tar"
-tar -cvf $BACKUP_DIR/$FILENAME $BASEDIR/production/couchdb/data/
+tar -cvf "${BACKUP_DIR}/${FILENAME}" "${BASE_DIR}/production/couchdb/data/"
 # TODO: store backup (where?)
 
 # delete files older than $STORAGE_LENGTH days
