1df1327b0d051a034b92735973cdd516e1192fbe
Fynn Heintz <fynn.heintz@gmail.com>
2020-08-22 14:41:45 +0200

    adjust config

--- website/frontend/src/config.js
+++ website/frontend/src/config.js
@@ -1,5 +1,5 @@
 export const WEBSITE_URL = 'https://text-stat.qu.tu-berlin.de'
 export const CONTACT_MAIL = 'f.heintz@campus.tu-berlin.de'
 export const COMPENSATION = 'TODO€ Amazon gift card'
-export const SESSIONS_PER_COMPENSATION = 'TODO'
-export const SESSION_DURATION = 10 // minutes
+export const SESSIONS_PER_COMPENSATION = '10'
+export const SESSION_DURATION = '5-10' // minutes
