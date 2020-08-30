86a961e402767c9ecba03ea9f28bc12645268232
Fynn Heintz <fynn.heintz@gmail.com>
2020-08-30 10:43:10 +0200

    display correct compensation amount & modalities

--- website/frontend/src/config.js
+++ website/frontend/src/config.js
@@ -1,5 +1,5 @@
 export const WEBSITE_URL = 'https://text-stat.qu.tu-berlin.de'
 export const CONTACT_MAIL = 'f.heintz@campus.tu-berlin.de'
-export const COMPENSATION = 'TODO€ Amazon gift card'
+export const COMPENSATION = '15€'
 export const SESSIONS_PER_COMPENSATION = '10'
 export const SESSION_DURATION = '5-10' // minutes
