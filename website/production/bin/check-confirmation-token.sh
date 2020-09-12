01e86fc7fa2aac8d6bf5a50edcfd47381389049e
Fynn Heintz <fynn.heintz@gmail.com>
2020-09-12 13:46:22 +0200

    add script to check confirmation tokens

---
+++ website/production/bin/check-confirmation-token.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+
+load DB admin password from env
+ENV_FILE="/home/adminuser/production/.env"
+export $(cat $ENV_FILE | xargs)
+
+encoded=$(echo "admin:${COUCHDB_PASSWORD}" | base64)
+authentication="${encoded:0:$((${#encoded}-1))}="
+
+echo "Please enter the participant ID: "
+read participant_id
+
+echo "Please enter a confirmation token for this participant: "
+read token
+while [[ $token != "end" ]]
+do
+    participant=$(curl -s http://localhost:5984/participants/${participant_id} -H "Authorization: Basic ${authentication}")
+    
+    regex_token="\\\"\d*\\\":\\\"$token\\\""
+    match=$(echo $participant | grep -o "$regex_token")
+
+    regex_session_id="\\\"\d*\\\""
+    session_id=$(echo $match | grep -o "$regex_session_id")
+
+    if [[ $session_id ]]; then
+    echo "👍🏻 This is the valid confirmation token for session $session_id";
+    else
+    echo "👎🏻 This token is not valid.";
+    fi
+
+    echo "Please enter another confirmation token or type 'end' to end the process: "
+    read token
+done
