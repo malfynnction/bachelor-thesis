be7f1d8ef0378eb3bafd4600366ef68130f77354
Fynn Heintz <fynn.heintz@gmail.com>
2020-09-12 16:05:07 +0200

    adjust script to work with new ratings format

--- data/extract-ratings-for-participant.js
+++ data/extract-ratings-for-participant.js
@@ -1,26 +1,10 @@
 const fs = require('fs')
 
 const rawData = fs.readFileSync('results/ratings.json')
-const ratings = JSON.parse(rawData).ratings
+const ratings = JSON.parse(rawData)
 
 module.exports = participantId => {
-  console.log(
-    'THIS RELIES ON THE OLD RATING FORMAT, THE RESULT IS PROBABLY NOT WHAT YOU WANT – TODO: FIX THIS'
-  )
-  const filtered = Object.keys(ratings)
-    .filter(item =>
-      ratings[item].allRatings.some(
-        allRatings => allRatings.participantId === participantId
-      )
-    )
-    .map(item => {
-      return {
-        item: item,
-        rating: ratings[item].allRatings.filter(
-          allRatings => allRatings.participantId === participantId
-        ),
-      }
-    })
+  const filtered = ratings.filter(item => item.participantId === participantId)
 
   fs.writeFileSync(
     `results/ratings-${participantId}.json`,
