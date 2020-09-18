61ef0ee58a1954fd1e6a0ee08bc68b60761737f6
Fynn Heintz <fynn.heintz@gmail.com>
2020-09-18 11:11:54 +0200

    Re-enable study (revert this when your study is over)
    Revert "study is finished - display only static page"
    
    This reverts commit 1e52eb928f3e72cf601f1e5c735ad4352160b388.
    
    Revert "remove backend"
    
    This reverts commit 2cf0d366d8b495b58e4ddbd928ea36f72bad1a89.
    
    Revert "no more need for backups"
    
    This reverts commit 71d1d7a73b6786112edbded0b1b2d75fd452ed46.
    
    Revert "remove all references to backend"
    
    This reverts commit bdcb43d227e63d6d829d11a8c7d2c8b9197f1a30.
    
    Revert "Revert "allow new signups and new sets of surveys - revert this when you have enough ratings""
    
    This reverts commit a05c17870344d25d9fca16fc0e3b2b1652de05ee.

--- website/frontend/src/components/Instructions.js
+++ website/frontend/src/components/Instructions.js
@@ -142,6 +142,13 @@ const Instructions = props => {
         ) : (
           <Fragment>
             <p>
+              If this is your first time participating in this study and you
+              don't have an ID yet, please click here:
+            </p>
+            <Link className="btn" to="/demographics">
+              New Participant
+            </Link>
+            <p>
               If you have already done a session in the past, please enter your
               participant ID and click "Start"
             </p>
@@ -173,10 +180,6 @@ const Instructions = props => {
                 Start
               </button>
             </form>
-            <p>
-              If you haven't participated before, you can no longer sign up,
-              since we already have enough participants. Thank you!
-            </p>
             {error.length > 0 ? (
               <div className="tu-border error-box background-pink">{error}</div>
             ) : null}
